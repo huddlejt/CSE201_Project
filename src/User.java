c61ef4b96ce73b0836aad7419bcb6daf499ca9f0
krumpejn <54953875+krumpejn@users.noreply.github.com>
2019-10-27 17:42:25 -0400

    Add files via upload
    added equals for user

--- src/User.java
+++ src/User.java
@@ -36,13 +36,41 @@ public class User {
 		this.userId = userId;
 	}
 	
+	// =================== equals =============================================
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (userId ^ (userId >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (userId != other.userId)
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
 	
 	// ==================== user library mgmt =================================
 	
-	
 	public boolean createItem(FoodItem newFood) {
 		
-		
+		///////////////////////////////////////
 		
 		return true;
 	}
